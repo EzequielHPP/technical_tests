0324c217c6c4a9fcbbfa21cd972d1e6b0e514188
Ezequiel Pereira <info@ezequielhpp.net>
2021-06-29 11:26:43 +0100

    Solved Perimeter Test
    Uses the Fibonacci sequence

---
+++ perimeter/code.php
@@ -0,0 +1,27 @@
+<?php
+include "../boot.php";
+
+function perimeter($n)
+{
+    $total = 0;
+    $cache = [0 => 0, 1 => 1];
+    for ($x = 0; $x <= $n+1; $x++) {
+        $value = calculate($x, $cache);
+        $total += $value;
+    }
+    return 4 * $total;
+}
+
+function calculate($n, &$cache){
+
+    if (!isset($cache[$n])) {
+        $cache[$n] = calculate($n - 1, $cache) + calculate($n - 2, $cache);
+    }
+    return $cache[$n];
+}
+
+$test = (new \App\Test());
+
+$test->assertEquals(80, perimeter(5), 'Failed: //result//');
+$test->assertEquals(216, perimeter(7), 'Failed: //result//');
+$test->assertEquals(3226062132197568, perimeter(70), 'Failed: //result//');
